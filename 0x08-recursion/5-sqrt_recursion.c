6c5bed1602559d5be6126577ca2e6eed6a9bfe9c
Ndigirigi Gichuhi <ndigirigigichuhi@gmail.com>
2023-06-07 23:22:26 +0300

    question 4

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -10,14 +10,14 @@
 int root(int num, int sqroot)
 {
 
-        if (sqroot % (num / sqroot) == 0)
-        {
-                if (sqroot * (num / sqroot) == num)
-                        return (sqroot);
-                else
-                        return (-1);
-        }
-        return (0 + root(num, sqroot + 1));
+	if (sqroot % (num / sqroot) == 0)
+	{
+		if (sqroot * (num / sqroot) == num)
+			return (sqroot);
+		else
+			return (-1);
+	}
+	return (0 + root(num, sqroot + 1));
 }
 
 
@@ -29,12 +29,12 @@ int root(int num, int sqroot)
 int _sqrt_recursion(int n)
 {
 
-        if (n < 0)
-                return (-1);
+	if (n < 0)
+		return (-1);
 
-        if (n == 1)
-                return (1);
+	if (n == 1)
+		return (1);
 
-        return (root(n, 2));
+	return (root(n, 2));
 }
 
