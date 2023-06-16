9aae96aae299ea60bf8de4a9f03fc3e748207a55
Ndigirigi Gichuhi <ndigirigigichuhi@gmail.com>
2023-06-16 05:56:10 +0300

    2

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -9,21 +9,21 @@
 
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
-	int *ptr;
-	unsigned int i;
+	char *array;
+        unsigned int i;
 
-	if (nmemb == 0 || size == 0)
+        if (nmemb == 0 || size == 0)
                 return (NULL);
 
-	ptr = malloc(nmemb * size);
+        array = malloc(nmemb * size);
 
-	if (ptr == NULL)
-		return (NULL);
+        if (array == NULL)
+                return (NULL);
 
-	for (i = 0; i < (size * nmemb); i++)
-	{
-		ptr[i] = 0;
-	}
+        for (i = 0; i < (size * nmemb); i++)
+        {
+                array[i] = 0;
+        }
 
-	return (ptr);
+        return (array);
 }
