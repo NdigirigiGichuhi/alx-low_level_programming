bdf400f99fac25f72987f644f1bccf391578bc81
Ndigirigi Gichuhi <ndigirigigichuhi@gmail.com>
2023-06-16 05:51:44 +0300

    2

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -20,7 +20,7 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 	if (nmemb == 0 || size == 0)
 		return (NULL);
 
-	for (i = 0; i < size; i++)
+	for (i = 0; i < size * nmemb; i++)
 	{
 		ptr[i] = 0;
 	}
