f69ab3da9debbc4aef597a7d9ca760fd7897e1fc
Ndigirigi Gichuhi <ndigirigigichuhi@gmail.com>
2023-07-13 13:43:52 +0300

    question 2

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -31,6 +31,9 @@ int create_file(const char *filename, char *text_content)
 	int fd;
 	ssize_t bytes_written;
 
+	if (filename == NULL)
+		return (-1);
+
 	fd = open(filename, O_RDWR | O_CREAT | O_TRUNC, 0600);
 
 	if (fd == -1)
