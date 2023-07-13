9b33ba3ebb2d33f606f789f9a5e810364b0f3e53
Ndigirigi Gichuhi <ndigirigigichuhi@gmail.com>
2023-07-13 17:54:55 +0300

    question 2

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -11,13 +11,13 @@ int append_text_to_file(const char *filename, char *text_content)
 {
 	int fd, len = 0, i = 0;
 	ssize_t written_chars;
-	
+
 	while (text_content[i] != '\0')
 	{
 		len++;
 		i++;
 	}
-		
+
 	if (filename == NULL)
 		return (-1);
 
@@ -33,4 +33,3 @@ int append_text_to_file(const char *filename, char *text_content)
 
 	return (1);
 }
-		
