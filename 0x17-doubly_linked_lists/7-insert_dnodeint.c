6d7c9001ec55b1e2a5c469bbea70b4882a74e938
NdigirigiGichuhi <ndigirigigichuhi@gmail.com>
2023-08-21 20:15:16 +0300

    question 7

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -31,7 +31,7 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	{
 		*h = newnode;
 	}
-	else if (idx > len || idx == 0)
+	else if (idx > len)
 	{
 		return (NULL);
 	}
