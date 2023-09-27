c46338315677429bfdf0bdf0dbe6319c3a51535d
NdigirigiGichuhi <ndigirigigichuhi@gmail.com>
2023-09-27 22:49:15 +0300

    3

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -1,52 +1,65 @@
+#include <stdlib.h>
+#include <string.h>
 #include "hash_tables.h"
 
-
 /**
- * hash_table_set - Add an element to the hash table
- * @ht: The hash table to add the key/value to
- * @key: The key (string) to add or update
- * @value: The value (string) associated with the key
+ * hash_table_set - Adds an element to a hash table.
+ * @ht: The hash table you want to add or update the key/value to.
+ * @key: The key (cannot be an empty string).
+ * @value: The value associated with the key (must be duplicated).
  *
- * Return: 1 if successful, 0 otherwise
+ * Returns: 1 if it succeeded, 0 otherwise.
+ * In case of collision, add the new node at the beginning of the list.
  */
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
     unsigned long int index;
-    hash_node_t *new_node = NULL, *current = NULL;
+    hash_node_t *new_node = NULL;
+    hash_node_t *temp = NULL;
 
-    if (ht == NULL || key == NULL || *key == '\0' || value == NULL)
+    if (ht == NULL || key == NULL || strlen(key) == 0)
         return (0);
 
-    index = key_index((unsigned char *)key, ht->size);
+    index = key_index((const unsigned char *)key, ht->size);
 
-    current = ht->array[index];
-    while (current != NULL)
+    temp = ht->array[index];
+
+    /* Check if the key already exists in the linked list */
+    while (temp != NULL)
     {
-        if (strcmp(current->key, key) == 0)
+        if (strcmp(temp->key, key) == 0)
         {
-            free(current->value);
-            current->value = strdup(value);
-            if (current->value == NULL)
+            /* Key found, update the value */
+            free(temp->value);
+            temp->value = strdup(value);
+            if (temp->value == NULL)
                 return (0);
             return (1);
         }
-        current = current->next;
+        temp = temp->next;
     }
 
+    /* Key not found, create a new node */
     new_node = malloc(sizeof(hash_node_t));
     if (new_node == NULL)
         return (0);
 
     new_node->key = strdup(key);
-    new_node->value = strdup(value);
-    if (new_node->key == NULL || new_node->value == NULL)
+    if (new_node->key == NULL)
     {
-        free(new_node->key);
-        free(new_node->value);
         free(new_node);
         return (0);
     }
 
+    new_node->value = strdup(value);
+    if (new_node->value == NULL)
+    {
+        free(new_node->key);
+        free(new_node);
+        return (0);
+    }
+
+    /* Add the new node to the beginning of the linked list */
     new_node->next = ht->array[index];
     ht->array[index] = new_node;
 
