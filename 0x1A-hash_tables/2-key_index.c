99a1861716be792fa1874e9714f0809e6741838d
NdigirigiGichuhi <ndigirigigichuhi@gmail.com>
2023-09-27 22:44:41 +0300

    2

--- 0x1A-hash_tables/2-key_index.c
+++ 0x1A-hash_tables/2-key_index.c
@@ -8,8 +8,9 @@
  */
 unsigned long int key_index(const unsigned char *key, unsigned long int size)
 {
-    unsigned long int hash_value;
-    hash_value = hash_djb2(key);
+	unsigned long int hash_value;
 
-    return (hash_value % size);
+	hash_value = hash_djb2(key);
+
+	return (hash_value % size);
 }
